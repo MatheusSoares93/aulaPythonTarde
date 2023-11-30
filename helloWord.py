2a4e2a93a3c5fc49c1313075686959ea6b53b1b1
MatheusSoares93 <142833871+MatheusSoares93@users.noreply.github.com>
2023-11-30 15:17:57 -0300

    Create helloWord.py

---
+++ helloWord.py
@@ -0,0 +1,8 @@
+print("Hello Word") #mostra na tela o texto hello word
+mensagem="Ola, tudo bem?"
+print(mensagem) #mostra a mensagem na tela
+print(mensagem[1]) #mostra a segunda letra da mensagem
+print(mensagem[0:4]) #mostra da primeira ate a 4 letra da mensagem
+print(mensagem[-1]) #mostra a ultima letra da mensagem
+print(mensagem[10:13])
+print(mensagem[-4:-1])
